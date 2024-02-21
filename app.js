5432843cafca834af0731a6d5c992577b8a2279c
Monfas <pedromonfort075@gmail.com>
2024-02-21 15:14:17 +0100

    ejs install module

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 const router = require('./routes/index')
-
+const ejs = require('ejs')
 const app = express()
 
 app.set('port', 5050)
